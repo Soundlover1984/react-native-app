32586e15781e787e889c54ca055d8cbe6376def9
Soundlover1984 <denys.kovtun@gmail.com>
2023-09-03 01:30:41 +0300

    module 2 finished

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from "react-native";
+import { useFonts } from "expo-font";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import PostsScreen from "./Screens/PostsScreen/PostsScreen";
+
+const App = () => {
+    const [fontsLoaded] = useFonts({
+        "Inter-Medium": require("./assets/fonts/Inter/static/Inter-Medium.ttf"),
+        "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+        "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+        "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require("./assets/images/app_background.jpg")}
+                resizeMode="cover"
+                style={styles.image}
+            />
+            {/* <RegistrationScreen /> */}
+            {/* <LoginScreen /> */}
+            <PostsScreen />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        position: "relative",
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    image: {
+        position: "absolute",
+        width: "100%",
+        flex: 1,
+        justifyContent: "center",
+    },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,16 @@
+Завдання​
+Створити папку Screens
+
+Створити компонент RegistrationScreen
+
+Створити компонент LoginScreen
+
+Створити екран PostsScreen
+
+Додати розмітку форми в компонент RegistrationScreen
+
+Додати розмітку форми в компонент LoginScreen
+
+Додати стилі до компонента RegistrationScreen
+
+Додати стилі до компонента LoginScreen
